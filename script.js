bbc810ae10f59d49a7d5720f8cd700c0d18cfc22
AlixiaDae <lizziejones1208@gmail.com>
2023-08-02 16:23:15 +0800

    Complete

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const rateBtns = document.querySelectorAll("li")
+let rate;
 
 rateBtns.forEach(el => {
     el.addEventListener("click", (e) => {
@@ -6,6 +7,27 @@ rateBtns.forEach(el => {
             button.classList.remove("clicked")
         })
         e.target.classList.add("clicked")
+        rate = e.target.textContent
     })
     
 })
+
+const submitRating = document.getElementById("submit-rating")
+const selectedRatingEl = document.querySelector(".selected-rating")
+
+submitRating.addEventListener("click", showThanks)
+
+function showThanks() {
+    if (!rate) {
+        return
+    }
+    const selectedArray = selectedRatingEl.textContent.split(" ")
+    selectedArray.splice(2, 0, `${rate}`)
+    selectedRatingEl.textContent = selectedArray.join(" ")
+
+    const thankCard = document.querySelector(".thank-card")
+    thankCard.classList.add("visible")
+
+    const ratingCard = document.querySelector(".rating-card")
+    ratingCard.classList.add("hidden")
+}
